98d5c540639734498ade09fb2da9e79f785a233c
alexisrodriguez99 <60524817+alexisrodriguez99@users.noreply.github.com>
2020-02-06 00:30:34 -0300

    Add files via upload
    tarea realizada en casa

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,12 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+    var importe;
+    var descuento;
+    var importeConDescuento;
+    importe=document.getElementById("importe").value;
+    importe=parseInt(importe);
+    descuento=importe*0.25;
+    importeConDescuento=importe-descuento;
+    document.getElementById("resultado").value=importeConDescuento;
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,15 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+var sueldo;
+var sueldoConAumento;
+var aumento;
+
+sueldo=document.getElementById("sueldo").value;
+sueldo=parseInt(sueldo);
+aumento=sueldo*0.1;
+sueldoConAumento=sueldo+aumento;
+document.getElementById("resultado").value=sueldoConAumento;
 
 	
 }
