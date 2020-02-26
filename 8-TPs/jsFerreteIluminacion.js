d6ebd4211f0face88edbe0486196cb5ee291305b
alumno <alumno>
2020-02-26 12:17:17 -0300

    echo en clase

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -92,3 +92,171 @@ function CalcularPrecio ()
           document.getElementById("precioDescuento").value=precioLampara;
      }
 }
+/*{
+var cantidad;
+var descuento;
+var precioFinal;
+var precioConDescuento;
+var marca;
+var precio;
+var impuesto;
+precio=35;
+cantidad=parseInt(document.getElementById(Cantidad).value);
+marca=document.getElementById(Marca).value;
+
+
+
+swtich(cantidad)
+{
+    case 1:
+    case 2:
+    {
+        descuento=0
+    }
+    case 3:
+    {
+        if (marca==ArgentinaLuz)
+        {
+            descuento=15;
+        }
+        else is (marca==FelipeLamparas)
+        {
+            descuento=10;
+        }
+        else 
+        {
+            descuento=5;
+        }
+    }
+    case 4:
+    {
+        if (marca==ArgentinaLuz || marca==FelipeLamparas)
+        {
+            descuento=25;
+        }
+        else 
+        {
+            descuento=20;
+        }
+    }
+    case 5
+    {
+        if (marca==ArgentinaLuz)
+        {
+            descuento=40;
+        }
+        else 
+        {
+            descuento=30;
+        }
+    }
+}
+
+precioConDescuento=precio*descuento/100;
+preciodFinal=precion-cantidad*precioConDescuento;
+document.getElementById("precioDescuento").value=precionConDescuento;
+if(precioFinal>120)
+{
+    impuesto=precio+precio*0.10;
+    precioFinal=impuesto+precio*cantidad;
+    alert("IIBB Usted pago "+impuesto);
+
+}
+
+
+
+}
+*/
+
+
+
+/*
+ECHO EN CLASE
+4.	Para el departamento de iluminación:
+Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
+A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
+B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
+C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
+D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
+E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
+ ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
+
+
+function CalcularPrecio() {
+    var cantidad;
+    var marca;
+    var precio = 35;
+    var porcDescuento;
+    var descuento;
+    var IIBB;
+    var importeFinal;
+    var precioConDescuento;
+
+
+    cantidad = parseInt(document.getElementById("Cantidad").value);
+    marca = document.getElementById("Marca").value;
+
+    if (cantidad >= 1) {
+
+        switch (cantidad) {
+            case 1:
+            case 2:
+                porcDescuento = 0;
+                break;
+            case 3:
+                if (marca == "ArgentinaLuz") {
+                    porcDescuento = 15;
+                } else if (marca == "FelipeLamparas") {
+                    porcDescuento = 10;
+                } else {
+                    porcDescuento = 5;
+                }
+                break;
+            case 4:
+                if (marca == "ArgentinaLuz" || marca == "FelipeLamparas") {
+                    porcDescuento = 25;
+                }
+                else {
+                    porcDescuento = 20;
+                }
+                break;
+            case 5:
+                if (marca == "ArgentinaLuz") {
+                    porcDescuento = 40;
+                }
+                else {
+                    porcDescuento = 30;
+                }
+                break;
+            default:
+                porcDescuento = 50;
+        }
+
+        // calculo el descuento
+        descuento = precio * porcDescuento / 100;
+        // al precio unitario le resto el descuento
+        precioConDescuento = precio - descuento;
+
+        // muestro el precio unitario en la caja inferior
+        document.getElementById("precioDescuento").value = precioConDescuento;
+
+        // ahora que ya se el precio de cada lampara
+        // calculo el total
+        importeFinal = precioConDescuento * cantidad;
+
+        if (importeFinal > 120) {
+            IIBB = importeFinal * 10 / 100;
+            importeFinal = importeFinal + IIBB;
+            alert("El importe final es $ " + importeFinal + "\nUsted pago $ " + IIBB + " de ingresos brutos");
+
+        } else {
+            alert("El importe final es $ " + importeFinal);
+        }
+
+    } else {
+        alert("No es una cantidad valida");
+    }
+
+
+}
+*/
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,46 @@
 function mostrar()
 {
+var nota;
+var sexo;
+var contador=0;
+var mayor;
+var sexoMayor;
+var menor;
+var sexoMenor;
+var contadorNota=0;
+for(id=0;id<5;id++)
+{
+    
+    nota=parseInt(prompt("Ingrese la nota"));
+    
+    while(nota<0 || nota>10)
+    {
+        nota=parseInt(prompt("NOTA invalida. Ingrese la nota"));
+    }
+    sexo=prompt("Ingrese el sexo, escribiendo solo 'f' o 'm'").toLowerCase();
+
+    while(sexo!="f" && sexo!="m")
+    {
+        sexo=prompt("Sexo Incorrecto. Ingrese el sexo, escribiendo 'f' o 'm'").toLowerCase();
+    }
+    if(id==0 || nota>mayor)
+    {
+        mayor=nota;
+        sexoMayor=sexo;
+    }
+    if(id==0 || nota<menor)
+    {
+        menor=nota;
+        sexoMenor=sexo;
+    }
+    if(sexo=="m" && nota>=6)
+    {
+        contador++;
+    }
+    contadorNota=contador+nota;
+}
+alert("El promedio de las notas es "+contadorNota/5);
+alert("La nota mas baja es "+menor+" y su sexo es "+sexoMenor);
+alert("La cantidad de varones que su nota es mayor o igual a 6 es: "+contador);
 
 }
