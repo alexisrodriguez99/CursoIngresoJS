df3110abe88a5032536a8d21663ac8ec54ef5c33
alumno <alumno@utnfra.com.ar>
2020-02-29 12:24:21 -0300

    parcialito

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,114 @@
 function mostrar()
 {
+/*
+b)Para la gestión de un hotel,
+ingresar los siguientes datos validados de una reserva
+nombre del huésped
+cantidad de personas
+cantidad de dia de estadia
+forma de pago(efectivo , tarjeta o QR)
+
+informar el huésped que trajo más personas en una sola reserva.
+la cantidad de personas que se quedaron más días
+la forma de pago más utilizada.
+el promedio de cantidad de días por reserva
+*/
+var nombre;
+var cantidadPersona;
+var cantidadDEstadia;
+var formaPago;
+var respuesta;
+var cantidadPersonaMayor;
+var flag=0;
+var nombreMayor;
+var cantidadDEstadiaMayor;
+var cantidadPersonaMayorEstadia;
+var contador=0;
+var promedio;
+var acumuladorDias=0;
+var contadorT=0;
+var contadorQ=0;
+var contadorE=0;
+var formaPagoMayorUsada;
+
+do{
+
+  nombre=prompt("Ingrese su nombre").toLowerCase();
+  while(nombre<'a' || nombre>'z')
+  {
+    nombre=prompt("Nombre invalido. Ingrese su nombre");
+  }
+
+
+  cantidadPersona=parseInt(prompt("Ingrese la cantidad de personas"));
+  while(isNaN(cantidadPersona) || cantidadPersona<=0)
+  {
+    cantidadPersona=parseInt(prompt("Cantidad invalida. Ingrese la cantidad de personas"))
+  }
+
+
+  formaPago=prompt("Ingrese la forma de pago: 'efectivo', 'tarjeta' o 'qr'").toLowerCase();    //isNaN()= no es un numero
+  while(formaPago!="efectivo" && formaPago!="tarjeta" && formaPago!="qr" || !isNaN(formaPago))//!isNaN() = es un numero
+  {
+    formaPago=prompt("Forma de pago incorrecta. Ingrese la forma de pago: 'efectivo', 'tarjeta' o 'QR'")
+  }
+
+
+  cantidadDEstadia=parseInt(prompt("Ingrese la cantidad de dia/dias de estadia"));
+  while(isNaN(cantidadDEstadia))
+  {
+    cantidadDEstadia=parseInt(prompt("Cantidad de dias invalida. Ingrese la cantidad de dia/dias de estadia"));
+  }
+
+  if(flag==0 || cantidadPersona>cantidadPersonaMayor)
+  {
+    nombreMayor=nombre;
+    cantidadPersonaMayor=cantidadPersona;
+
+  }
+  if (flag==0 || cantidadDEstadia>cantidadDEstadiaMayor)
+  {
+    cantidadPersonaMayorEstadia=cantidadPersona;
+    cantidadDEstadiaMayor=cantidadDEstadia;
+    flag=1;
+  }
+
+  if(formaPago=="efectivo")
+  {
+    contadorE++;
+  }
+  else if (formaPago=="tarjeta")
+  {
+    contadorT++;
+  }
+  else
+  {
+    contadorQ++;
+  }
+
+
+  acumuladorDias=acumuladorDias+cantidadDEstadia;
+  contador++;
+    respuesta=prompt("si quiere ingresar mas datos escriba 'si'");
+}while(respuesta=="si");
+
+
+if(contadorE>contadorQ && contadorE>contadorT)
+  {
+    formaPagoMayorUsada="efectivo"
+  }
+else if(contadorT>contadorQ)
+  {
+    formaPagoMayorUsada="tarjeta"
+  }
+else{
+  formaPagoMayorUsada="qr"
+}
+
+promedio=acumuladorDias/contador;
+document.write("el hueped que trajo mas personas en una solo reserva es: "+nombreMayor+" y la cantidad de personas fueron: "+cantidadPersonaMayor);
+document.write("</br>la cantidad de personas que se quedaron mas dias es de "+cantidadPersonaMayorEstadia+" y su cantidad de dia es de "+cantidadDEstadiaMayor+"dias");
+document.write("</br>la forma de pago mas utilizada es: "+formaPagoMayorUsada);
+document.write("</br>el promedio de cantidad de dias por reserva es de: "+promedio);
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
@@ -1,4 +1,102 @@
 function mostrar()
 {
+/*
+c)Se pide la carga de datos de un contenedor  de alimento canino con un máximo de carga de 1000kg,
+cargar hasta que el cliente quiera o se llene el contenedor.
+se pide:
+marca
+kilos por bolsa
+cantidad de bolsa
+importe por bolsa
 
+que marca tiene más kilos en el contenedor
+que marca tiene más bolsas de alimento en el contenedor
+que marca tiene el mayor importe por bolsa de alimento
+el importe y la marca de la bolsa de alimento menos pesada.
+
+*/
+
+var marca;
+var kilos;
+var cantidadBolsas;
+var ImporteBolsas;
+var contenedor=1000;
+var respuesta;
+var contador=0;
+var kilosMax;
+var cantidadBolsasMax;
+var marcaMaxKilos;
+var marcaMaxBolsas;
+var marcaMaxImporte;
+var ImporteBolsasMax;
+var importeMenor;
+var marcaMenor;
+var kilosMin;
+var acumuladorKilos=0;
+do
+{
+
+  if(contenedor<acumuladorKilos)
+  {
+    break;
+  }
+  marca=prompt("Ingrese una marca").toLowercase();
+  while(marca<'a' || marca>'z')
+  {
+    marca=prompt("Marca invalida. Ingrese una marca");
+  }
+
+  kilos=parseInt(prompt("Ingrese los kilos por bolsa"));
+  while(isNaN(kilos) || kilos<=0)
+  {
+    kilos=parseInt(prompt("Error. Ingrese los kilos por bolsa"));
+
+  }
+
+  cantidadBolsas=parseInt(prompt("Ingrese la cantidad de bolsa"));
+  while(isNaN(cantidadBolsas) || cantidadBolsas<=0)
+  {
+    cantidadBolsas=parseInt(prompt("Ingrese la cantidad de bolsa"));
+  }
+
+  ImporteBolsas=parseInt(prompt("Ingrese el importe por bolsa"));
+  while(isNaN(ImporteBolsas) || ImporteBolsas<=0)
+  {
+    ImporteBolsas=parseInt(prompt("Ingrese el importe por bolsa"));
+  }
+
+   if(kilos>kilosMax || contador==0)
+   {
+     kilosMax=kilos;
+     marcaMaxKilos=marca;
+   }
+
+   if(cantidadBolsas>cantidadBolsasMax || contador==0)
+   {
+     marcaMaxBolsas=marca;
+     cantidadBolsasMax=cantidadBolsas;
+   }
+
+   if(ImporteBolsas>ImporteBolsasMax || contador==0)
+   {
+     ImporteBolsasMax=ImporteBolsas;
+     marcaMaxImporte=marca;
+   }
+
+   if(kilos<kilosMin || contador==0)
+   {
+     kilosMin=kilos;
+     importeMenor=ImporteBolsas;
+     marcaMenor=marca;
+   }
+
+   acumuladorKilos=acumuladorKilos+kilos;
+   contador++;
+  respuesta=prompt("Ingrese 's' para ingresar mas datos");
+}while(respuesta=="s");
+
+document.write("la marca que tiene mas kilos es "+marcaMaxKilos+" y su cantidad de kilos es de "+kilosMax);
+document.write("</br>la marca que tiene mas bolsas de alimento es "+marcaMaxBolsas+" y su cantidad de bolsas es de "+cantidadBolsasMax);
+document.write("</br>la marca que tiene el mayor importe por bolsa es "+marcaMaxImporte+" y su iporte es de "+ImporteBolsasMax);
+document.write("</br>el importe y la marca de la bolsa de aliemto menos pesada es de "+importeMenor+"$ , su marca es "+marcaMenor+" y el peso de la bolsa es de "+kilosMin);
 }
